67bfec4c9208fe8f30e22a800ae1ada90d96a27d
Tom Sturge <tomsturge+github@gmail.com>
2020-05-07 11:38:07 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 <h1 align="center">
   🔍 <br/>
-  pkg-Lookup
+  pkg-lookup
 
 [![npm](https://img.shields.io/npm/v/pkg-lookup.svg?style=flat-square)](https://www.npmjs.com/package/pkg-lookup)
 [![npm bundle size (minified + gzip)](https://img.shields.io/bundlephobia/minzip/pkg-lookup.svg?style=flat-square)](https://www.npmjs.com/package/pkg-lookup)
-[![Maintainability](https://api.codeclimate.com/v1/badges/9e1c94798c83e9d394df/maintainability)](https://codeclimate.com/github/mohtasmedia/pkg-lookup/maintainability)
+[![Maintainability](https://api.codeclimate.com/v1/badges/05e9e7a0d769002e1b89/maintainability)](https://codeclimate.com/github/mohtasmedia/pkg-lookup/maintainability)
 ![](https://img.shields.io/badge/licence-MIT-blue.svg?style=flat-square)
 [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
 
@@ -24,7 +24,7 @@ npm install -g pkg-lookup
 
 ## Usage
 
-The easiest way to use PkgLookup is via `npx`, the below command will return a short summary of the package, including is size (minifed and gzipped).
+The easiest way to use pkg-lookup is via `npx`, the below command will return a short summary of the package, including is size (minifed and gzipped).
 
 ```sh
 npx pkg-lookup react
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 "use strict";
 const https = require("https");
-const [pkgName, ...flags] = process.argv.slice(2);
+const [pkgName] = process.argv.slice(2);
+const colors = {
+  r: "\x1b[0m",
+  y: "\x1b[33m",
+  d: "\x1b[2m",
+};
 const request = ([hostname, path]) =>
   new Promise((resolve, reject) => {
     https.get({ hostname, path, headers: { "User-Agent": "" } }, (response) => {
@@ -11,81 +16,57 @@ const request = ([hostname, path]) =>
       response.on("error", (error) => reject(error));
     });
   }).catch((e) => console.error(e));
-const color = (v, c) => {
-  const colors = {
-    reset: "\x1b[0m",
-    underline: "\x1b[4m",
-    red: "\x1b[31m",
-    green: "\x1b[32m",
-    yellow: "\x1b[33m",
-    cyan: "\x1b[36m",
-    dim: "\x1b[2m",
-  };
-
-  return `${colors[c] + v + colors.reset}`;
-};
-const label = (v) => `${color(v, "yellow")}: `;
+const color = (v, c) => `${colors[c] + v + colors.r}`;
+const label = (v) => `${color(v, "y")}: `;
 const date = (v) => `${v.getDate()}/${v.getMonth() + 1}/${v.getFullYear()}`;
 const formatBytes = (v) => (v >= 1024 ? `${(v / 1024).toFixed(1)}KB` : `${v}B`);
-const log = (v) => console.log(`    ${v ? v : ""}`);
+const log = (v) => console.log(v ? `    ${v}` : "");
 
 (async () => {
-  try {
-    const npm = await request([
-      "api.npms.io",
-      `/v2/package/${encodeURIComponent(pkgName)}`,
-    ]);
-
-    if (npm.code === "NOT_FOUND") {
-      return console.log(npm.message);
-    }
-
-    const {
-      collected: {
-        metadata,
-        github,
-        npm: { downloads },
-      },
-    } = npm;
-    const bp = await request([
-      "bundlephobia.com",
-      `/api/size?package=${pkgName}`,
-    ]);
-    const repo = metadata.links.repository.split("/");
-    const { total_count: prCount } = await request([
-      "api.github.com",
-      `/search/issues?q=repo:${repo[3]}/${repo[4]}+is:pr+state:open&per_page=1`,
-    ]);
-
-    log();
-    log(
-      `${metadata.name} - v${metadata.version} (Published ${date(
-        new Date(metadata.date)
-      )})`
-    );
-    log(color(metadata.description, "dim"));
-    log();
-    log(
-      `${label("Size / Gzipped") + formatBytes(bp.size)} / ${formatBytes(
-        bp.gzip
-      )}`
-    );
-    log(
-      `${
-        label("Total Downloads") +
-        downloads[downloads.length - 1].count.toLocaleString()
-      }`
-    );
-    log(`${label("Open issues / PRs") + github.issues.openCount} / ${prCount}`);
-    log(`${label("Github Stars") + github.starsCount.toLocaleString()}`);
-    log();
-    log(`${label("NPM") + color(metadata.links.npm, "underline")}`);
-    log(`${label("Homepage") + color(metadata.links.homepage, "underline")}`);
-    log(`${label("Repo") + color(metadata.links.repository, "underline")}`);
-    log();
-    log(`${label("Liscnce") + metadata.license}`);
-    log();
-  } catch (e) {
-    console.error(e);
+  const npms = await request([
+    "api.npms.io",
+    `/v2/package/${encodeURIComponent(pkgName)}`,
+  ]);
+  if (npms.code) {
+    return log(npms.message);
   }
+  const {
+    collected: { metadata, github, npm },
+  } = npms;
+  const bp = await request([
+    "bundlephobia.com",
+    `/api/size?package=${pkgName}`,
+  ]);
+  const repo = metadata.links.repository.split("/");
+  const { total_count: prCount } = await request([
+    "api.github.com",
+    `/search/issues?q=repo:${repo[3]}/${repo[4]}+is:pr+state:open&per_page=1`,
+  ]);
+  log();
+  log(
+    `${metadata.name} - v${metadata.version} (Published ${date(
+      new Date(metadata.date)
+    )})`
+  );
+  log(color(metadata.description, "d"));
+  log();
+  log(
+    `${label("Size / Gzipped") + formatBytes(bp.size)} / ${formatBytes(
+      bp.gzip
+    )}`
+  );
+  log(
+    `${
+      label("Downloads") +
+      npm.downloads[npm.downloads.length - 1].count.toLocaleString()
+    }`
+  );
+  log(`${label("Open issues / PRs") + github.issues.openCount} / ${prCount}`);
+  log(`${label("Github Stars") + github.starsCount.toLocaleString()}`);
+  log();
+  log(`${label("NPM") + metadata.links.npm}`);
+  log(`${label("Homepage") + metadata.links.homepage}`);
+  log(`${label("Repo") + metadata.links.repository}`);
+  log();
+  log(`${label("Liscnce") + metadata.license}`);
 })();
