8dfbe34784a224eb38f85d51b9b88f3b5ce7a307
Tom Sturge <tomsturge+github@gmail.com>
2020-05-07 12:57:55 +0100

    Give output some breathing room

--- src/cli.js
+++ src/cli.js
@@ -17,7 +17,7 @@ const request = ([hostname, path]) =>
     });
   }).catch((e) => console.error(e));
 const color = (v, c) => `${colors[c] + v + colors.r}`;
-const label = (v) => `${color(v, "y")}: `;
+const label = (v) => `${color(v, "y")} `;
 const date = (v) => `${v.getDate()}/${v.getMonth() + 1}/${v.getFullYear()}`;
 const formatBytes = (v) => (v >= 1024 ? `${(v / 1024).toFixed(1)}KB` : `${v}B`);
 const log = (v) => console.log(v ? `    ${v}` : "");
@@ -51,22 +51,21 @@ const log = (v) => console.log(v ? `    ${v}` : "");
   log(color(metadata.description, "d"));
   log();
   log(
-    `${label("Size / Gzipped") + formatBytes(bp.size)} / ${formatBytes(
+    `${label("Size / Gzipped")}   ${formatBytes(bp.size)} / ${formatBytes(
       bp.gzip
     )}`
   );
   log(
-    `${
-      label("Downloads") +
-      npm.downloads[npm.downloads.length - 1].count.toLocaleString()
-    }`
+    `${label("Downloads")}        ${npm.downloads[
+      npm.downloads.length - 1
+    ].count.toLocaleString()}`
   );
   log(`${label("Open issues / PRs") + github.issues.openCount} / ${prCount}`);
-  log(`${label("Github Stars") + github.starsCount.toLocaleString()}`);
+  log(`${label("Github Stars")}     ${github.starsCount.toLocaleString()}`);
   log();
-  log(`${label("NPM") + metadata.links.npm}`);
+  log(`${label("NPM")}     ${metadata.links.npm}`);
   log(`${label("Homepage") + metadata.links.homepage}`);
-  log(`${label("Repo") + metadata.links.repository}`);
+  log(`${label("Repo")}    ${metadata.links.repository}`);
   log();
   log(`${label("Liscnce") + metadata.license}`);
 })();
