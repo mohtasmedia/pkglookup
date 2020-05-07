81633b55d46284aeae7e1ffeb114d785be46adea
Tom Sturge <44009+tomsturge@users.noreply.github.com>
2020-05-07 11:38:07 +0100

    Update readme

--- rollup.config.js
+++ rollup.config.js
@@ -7,9 +7,5 @@ const pkg = JSON.parse(fs.readFileSync("./package.json"));
 export default {
   input: "src/cli.js",
   plugins: [terser(), preserveShebangs()],
-  output: [
-    { file: pkg.main, format: "cjs" },
-    { file: pkg.module, format: "es" },
-    { file: pkg["umd:main"], format: "umd", name: pkg.name },
-  ],
+  output: [{ file: pkg.main, format: "cjs" }],
 };
