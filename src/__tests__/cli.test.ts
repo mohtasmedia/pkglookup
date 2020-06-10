309abde9e1f9a1043c2dc17a277cb3cd27e6da61
tomsturge <tom@mohtas.media>
2020-06-10 16:33:36 +0100

    Add test coverage to codeclimate

--- src/__tests__/cli.test.ts
+++ src/__tests__/cli.test.ts
@@ -3,11 +3,11 @@ import { cli } from "../utils";
 test("Output expected amount of console.logs", async () => {
   console.log = jest.fn();
   await cli(["", "", "preact"]);
-  expect(console.log).toHaveBeenCalledTimes(14);
+  expect(console.log).toHaveBeenCalledTimes(15);
 });
 
-test("Prompt for package name", async () => {
+test("Handle no package name", async () => {
   console.log = jest.fn();
-  await cli([""]);
-  expect(console.log).toHaveBeenCalledWith("Provide name of package: ");
+  await cli([]);
+  expect(console.log).toHaveBeenCalledTimes(1);
 });
